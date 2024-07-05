d81b3c23c6d013f77770d461bec86b505040ea1f
katyakolataeva@icloud.com <katyakolataeva@icloud.com>
2024-07-05 10:47:32 +0300

    first

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+function checkLength (length, maxLength) {
+  if (length <= maxLength) {
+    return true;
+  }else {
+    return false;
+  }
+}
+checkLength(10,15);
+
+
+function checkPalindromy (length) {
+  return length.split('').reverse().join('') === length;
+}
+checkPalindromy('дед');
