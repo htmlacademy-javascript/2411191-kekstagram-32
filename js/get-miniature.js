af8062255708ec799f0a819f3838782a3616bea4
katyakolataeva@icloud.com <katyakolataeva@icloud.com>
2024-07-30 18:39:52 +0300

    fix77

--- js/get-miniature.js
+++ js/get-miniature.js
@@ -5,14 +5,14 @@ const fragment = document.createDocumentFragment();
 const arrayDataMiniature = getPictures(25);
 
 
-arrayDataMiniature.forEach(({url, likes, comments}) => {
+arrayDataMiniature.forEach(({url, likes, comments, description}) => {
 
   const photoElement = drawMiniature.cloneNode(true);
 
   photoElement.querySelector('.picture__img').src = url;
   photoElement.querySelector('.picture__comments').textContent = comments.length;
   photoElement.querySelector('.picture__likes').textContent = likes;
-
+  photoElement.querySelector('.picture__img').alt = description;
   fragment.append(photoElement);
 
 });
